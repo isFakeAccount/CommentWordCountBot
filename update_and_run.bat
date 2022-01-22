672d8c9fd976c45a14376d8ddefb8c77b6feb25e
isFakeAccount <trevorphillips@gmx.us>
2022-01-22 05:32:21 -0500

    Added bat file to update and run the bot.

---
+++ update_and_run.bat
@@ -0,0 +1,12 @@
+git pull
+venv\Scripts\pip install -r requirements.txt
+venv\Scripts\pip install --upgrade pip
+
+if exist config.yaml (
+	venv\Scripts\python main.py
+	pause
+) else (
+	echo "config.yaml file not found. Please create the file from example_config.yaml"
+	pause
+	exit \b -1
+)
